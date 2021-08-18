ba317f7ff93c9165289bcc697801a1ad501b841a
willasur <87398003+willasur@users.noreply.github.com>
2021-08-18 15:04:04 -0700

    Add variable num1 and num2

--- Program.cs
+++ Program.cs
@@ -7,20 +7,29 @@ namespace CalulatorProject
         static void Main(string[] args)
         {
 
-
+           
 
             Console.WriteLine("Console Calculator");
             //ask user for a number
 
             Console.WriteLine("Enter a Number:");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Enter another Number:");
 
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
             //user options
-            Console.WriteLine($"Please Select An Operation");
+            Console.WriteLine($"Please Select An Operation:");
             Console.WriteLine($"Add");
             Console.WriteLine($"Subtract");
             Console.WriteLine($"Multiply");
             Console.WriteLine($"Divide");
+
+            //user selection
+            Console.WriteLine($"Operation:");
+
 
             {
 
