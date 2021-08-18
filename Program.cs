c7739c530697614d4590a4766cb734f627b2ba46
willasur <87398003+willasur@users.noreply.github.com>
2021-08-18 14:33:35 -0700

    Add ask user for numbers

--- Program.cs
+++ Program.cs
@@ -5,13 +5,23 @@ namespace CalulatorProject
     class Program
     {
         static void Main(string[] args)
+        {
 
 
-            
-        { 
+
+            Console.WriteLine("Console Calculator");
             //ask user for a number
-            Console.WriteLine("Please enter a number");
-           
+
+            Console.WriteLine("Enter a Number:");
+            Console.WriteLine("Enter another Number:");
+
+            {
+
+
+            }
         }
+
     }
 }
+    
+
