df4c0fb9839c5fea23417df1c8c27b8178c44eda
willasur <87398003+willasur@users.noreply.github.com>
2021-08-23 20:12:36 -0700

    Add try, catch, (Format,DivideByZero Exceptions)

--- Program.cs
+++ Program.cs
@@ -7,68 +7,102 @@ namespace CalulatorProject
         static void Main(string[] args)
         {
 
-           
+
+
+
+            int num1;
+            int num2;
+
+
 
             Console.WriteLine("Console Calculator");
 
-
-            //ask user for a number
-            Console.WriteLine("Enter a Number:");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-
-            //ask user for second number
-            Console.WriteLine("Enter another Number:");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            //user options
-            Console.WriteLine("Please Select An Operation:");
-            Console.WriteLine("Add");
-            Console.WriteLine("Subtract");
-            Console.WriteLine("Multiply");
-            Console.WriteLine("Divide");
-
-            //user select option
-            Console.Write("Operation: ");
-            //user input stored in variable operation
-            string operation = Console.ReadLine();
-
-
-            //user selection and equation
-            Console.WriteLine(num1 + (operation)  + num2 + " is equal to: ");
-
-            //operation
-
-            if (operation == "+")
+            try
             {
-                Console.WriteLine(num1 + num2);
+
+                //ask user for a number
+                Console.WriteLine("Enter another Number:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                //ask user for second number
+                Console.WriteLine("Enter another Number:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+                //user options
+                Console.WriteLine("Please Select An Operation:");
+                Console.WriteLine("Add");
+                Console.WriteLine("Subtract");
+                Console.WriteLine("Multiply");
+                Console.WriteLine("Divide");
+
+                //user select option
+                Console.Write("Operation: ");
+
+                //user input stored in variable operation
+                string operation = Console.ReadLine();
+
+
+                //user selection and equation
+                string equal = " is equal to:";
+                string errormesg = " is an invalid equation";
+
+
+
+
+                //operation
+
+
+                if (operation == "+")
+                {
+                    Console.WriteLine(num1 + (operation) + num2 + equal);
+                    Console.WriteLine(num1 + num2);
+                }
+                else if (operation == "-")
+                {
+                    Console.WriteLine(num1 + (operation) + num2 + equal);
+                    Console.WriteLine(num1 - num2);
+                }
+                else if (operation == "*")
+                {
+                    Console.WriteLine(num1 + (operation) + num2 + equal);
+                    Console.WriteLine(num1 * num2);
+                }
+                else if (operation == "/")
+                {
+                    Console.WriteLine(num1 + (operation) + num2 + equal);
+                    Console.WriteLine(num1 / num2);
+                }
+                //error message for invalid operator
+
+                else Console.WriteLine(num1 + (operation) + num2 + errormesg);
+
+                Console.ReadLine();
+
             }
-            if (operation == "-")
+
+            catch (FormatException)
             {
-                Console.WriteLine(num1 - num2);
+                Console.WriteLine("Numbers Only");
             }
-            if (operation == "*")
+            catch (DivideByZeroException)
             {
-                Console.WriteLine(num1 * num2);
-            }
-            if (operation == "/")
-            {
-                Console.WriteLine(num1 / num2);
+                Console.WriteLine("0 is not good");
             }
 
-
-
-
-
+            Console.ReadKey();
         }
 
-            }
+    }
+          
 
-
-
-            
-
-
-            }
+}
         
 
     
